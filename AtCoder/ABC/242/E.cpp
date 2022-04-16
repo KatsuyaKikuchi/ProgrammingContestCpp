6621cb41657a0d130a9d6386c3c7661c07624aab
KatsuyaKikuchi <k.kikuchi.ah@gmail.com>
2022-04-16 23:27:24 +0900

    solve abc

---
+++ AtCoder/ABC/242/E.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long int;
+using pll = pair<ll, ll>;
+
+#define FOR(i, n, m) for(ll i = (m);(i)<(n);++(i))
+#define REP(i, n) FOR(i,n,0)
+#define OF64 std::setprecision(40)
+
+const ll MOD = 998244353;
+const ll INF = (ll) 1e15;
+const long double PI = 3.1415926535897932384626433832795028841971;
+
+template<std::uint_fast64_t Modulus>
+class ModInt {
+    using u64 = std::uint_fast64_t;
+public:
+    constexpr ModInt(u64 value = 0) noexcept:
+            mValue(value % Modulus) {}
+
+    u64 &value() { return mValue; }
+
+    constexpr ModInt &operator+=(ModInt v) noexcept {
+        mValue += v.mValue;
+        if (mValue >= Modulus)
+            mValue -= Modulus;
+        return *this;
+    }
+
+    constexpr ModInt &operator-=(ModInt v) noexcept {
+        if (mValue < v.mValue)
+            mValue += Modulus;
+        mValue -= v.mValue;
+        return *this;
+    }
+
+    constexpr ModInt &operator*=(ModInt v) noexcept {
+        mValue = (mValue * v.mValue) % Modulus;
+        return *this;
+    }
+
+    constexpr ModInt &operator/=(ModInt v) noexcept {
+        *this *= v.pow(Modulus - 2);
+        return *this;
+    }
+
+    constexpr ModInt operator+(ModInt v) noexcept {
+        return ModInt(*this) += v;
+    }
+
+    constexpr ModInt operator-(ModInt v) noexcept {
+        return ModInt(*this) -= v;
+    }
+
+    constexpr ModInt operator*(ModInt v) noexcept {
+        return ModInt(*this) *= v;
+    }
+
+    constexpr ModInt operator/(ModInt v) noexcept {
+        return ModInt(*this) /= v;
+    }
+
+    ModInt pow(u64 r) {
+        u64 p = mValue;
+        ModInt ret(1);
+        while (r > 0) {
+            if (r & 1)
+                ret *= p;
+            r >>= 1;
+            p = (p * p) % Modulus;
+        }
+        return ret;
+    }
+
+private:
+    u64 mValue;
+};
+
+using mint = ModInt<MOD>;
+
+ll solve() {
+    ll N;
+    cin >> N;
+    string S;
+    cin >> S;
+    ll M = (S.length() + 1) / 2;
+    mint ans = 0;
+    string s(N, 'Z');
+    REP(i, M) {
+        REP(j, S[i] - 'A') {
+            ll n = (N - (i + 1) * 2 + 1) / 2;
+            ans += ((mint) 26).pow(n);
+        }
+        s[i] = s[N - 1 - i] = S[i];
+    }
+
+    if (s <= S)
+        ans += 1;
+    return ans.value();
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    ll T;
+    cin >> T;
+    REP(_, T) {
+        cout << solve() << endl;
+    }
+    return 0;
+}
